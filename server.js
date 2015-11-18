d783ca171d0daf27cfe95b210804b283e0441e6f
Alexander Georgie <al.georgie@gmail.com>
2015-11-17 16:04:27 -0800

    Added in the hakunamatata path

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ app.get('/', function(req, res){
 	res.send("Hello World");
 });
 
+app.get("/hakunamatata", function(req, res){
+	print req;
+})
+
 var server = app.listen(8888, function(){
 	var host = server.address().address;
 	var port = server.address().port;
 
 	console.log("Code Highlighting service is listening at http://%s:%s", host, port);
-	console.log("Does this work?");
-	console.log(highlight);
 });
