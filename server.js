63bd07cfa83262e8eb0bfbdb5a5ef8ce1a932717
Alexander Georgie <al.georgie@gmail.com>
2015-11-18 11:05:31 -0800

    Add in debug statement instead of return

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post("/hakunamatata", function(req, res){
 	console.log(req.body);
 	var code = req.body["code"];
 	formattedCode = "<pre><code>"+code+"</code></pre>";
-	res.send(highlight.highlightAuto(formattedCode));
+	console.log(highlight.highlightAuto(formattedCode));
 })
 
 var server = app.listen(8888, function(){
